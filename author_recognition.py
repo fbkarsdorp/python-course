c9cdbe683e582980d6f9a5d231c22b74d1f6d087
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-03-21 15:57:47 +0100

    author recognition

---
+++ author_recognition.py
@@ -0,0 +1,19 @@
+
+from math import log
+
+def train(corpus):
+	author_feature_counts = {}
+	feature_counts = defaultdict(int)
+	for author, features in corpus.iteritems():
+		author_feature_counts[author] = features
+			feature_author_counts[feature][author] = count
+	return feature_counts, feature_author_counts
+
+def test(document, feature_counts, feature_author_counts):
+	scores = defaultdict(float)
+	for feature in document:
+		for author in model:
+			scores[author] += math.log(
+				(feature_author_counts[feature][author] + 1.0) / 
+				(feature_counts[feature] + len(feature_counts)))
+	return max(scores, key=scores.__getitem__, reverse=True)
