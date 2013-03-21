9a0d08adf6e9ec163929d1b5df88c194a669913d
Maarten van Gompel <proycon@anaproy.nl>
2013-03-21 19:01:36 +0100

    initial addition of simple tokeniser, sentence splitter, and file reader

--- author_recognition.py
+++ author_recognition.py
@@ -20,3 +20,52 @@ def test(document, feature_counts, feature_author_counts):
                 (feature_counts[feature] + len(feature_counts)))
     return max(scores, key=scores.__getitem__)
 
+
+def readcorpusfile(filename):
+    f = open(filename,'rt',encoding='utf-8')
+    text = f.read()
+    f.close()
+    return text
+
+          
+def tokenizer(text,separatorlist, abbreviationlist):
+    tokens = []
+    begin = 0
+    for i, c in enumerate(text):
+        if c in separatorlist:
+            token = text[begin:i]
+            if c == '.' and token in abbreviationlist:
+                #this is an abbreviation ending in a period, do not split the period
+                tokens.append(token + c) 
+            elif (c == '.' or c == ',') and token.isdigit() and (i < len(tokens) - 1 and tokens[i+1].isdigit()):
+                #period is probably a decimal or thousands separator, let's keep these together and just continue with the next
+                continue
+            elif c != " " and c != "\n": 
+                tokens.append(token)
+                tokens.append(c) #anything but spaces and newlines (i.e. punctuation) counts as a token too
+            begin = i+1 #set the begin cursor
+    return tokens
+            
+            
+
+def getsentences(tokens):
+    sentences = []
+    begin = 0
+    for i, token in enumerate(tokens):
+        #is this an end-of-sentence marker ... and is this either the last token or the next token is NOT an end of sentence marker as well? (to deal with ellipsis etc)
+        if token in ('.','?','!')      and (i == len(tokens) - 1 or not tokens[i+1] in ('.','?','!')): 
+            sentences.append( tokens[begin:i+1] )
+            begin = i+1
+    return sentences
+            
+            
+       
+    
+        
+            
+            
+            
+        
+        
+    
+    
