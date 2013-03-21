64344c0ded1115ec6fdc80ba671d992bc85e3ef4
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-03-21 16:15:59 +0100

    pffff

--- author_recognition.py
+++ author_recognition.py
@@ -3,20 +3,20 @@ from collections import defaultdict
 from math import log
 
 def train(corpus):
-    author_feature_counts = {}
+    feature_author_counts = {}
     feature_counts = defaultdict(int)
     for author, features in corpus.iteritems():
-        author_feature_counts[author] = features
+        feature_author_counts[author] = features
         for feature, count in features.iteritems():
-            feature_author_counts[feature][author] = count
+            feature_counts[feature] = count
     return feature_counts, feature_author_counts
 
 def test(document, feature_counts, feature_author_counts):
     scores = defaultdict(float)
     for feature in document:
-        for author in model:
-            scores[author] += math.log(
-                (feature_author_counts[feature][author] + 1.0) / 
+        for author in feature_author_counts:
+            scores[author] += log(
+                (feature_author_counts[author][feature] + 1.0) / 
                 (feature_counts[feature] + len(feature_counts)))
     return max(scores, key=scores.__getitem__)
 
