0ae8cd594e7b4c462c5e5edc06372197f6049dd4
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-03-22 09:15:38 +0100

    documentation

--- pig_latin.py
+++ pig_latin.py
@@ -1,16 +1,23 @@
+#! /usr/bin/env python3
+# -*- coding: utf8 -*-
+
 
 VOWELS = 'aeiouAEIOU'
 
 def starts_with_vowel(word):
+    "Does the word start with a vowel?"
     return word[0] in VOWELS
 
 def add_ay(word):
+    "Add 'ay' at the end of the word."
     return word + 'ay'
 
 def convert_word(word):
+    "Convert a word to latin."
     if not starts_with_vowel(word):
         return convert_word(word[1:] + word[0])
     return add_ay(word)
 
 def convert_text(text):
-    return ' '.join(convert_word(word) for word in text)
+    "Convert all words in a sentence to latin."
+    return ' '.join(convert_word(word) for word in text.split())
