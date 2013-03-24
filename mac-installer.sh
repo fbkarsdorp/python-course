4e9b0b9b1f4b41588306757d484bbd985ac41349
Maarten van Gompel <proycon@pegasus.local>
2013-03-24 22:07:46 +0100

    fix

--- mac-installer.sh
+++ mac-installer.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+
 mkdir tmp
 cd tmp
-curl http://python.org/ftp/python/3.3.0/python-3.3.0-macosx10.6.dmg
+curl http://python.org/ftp/python/3.3.0/python-3.3.0-macosx10.6.dmg > python-3.3.0-macosx10.6.dmg
 hdiutil attach python-3.3.0-macosx10.6.dmg
 cd /Volumes/Python\ 3.3.0
 sudo installer -pkg Python.mpkg -target /
 cd -
-curl https://pypi.python.org/packages/source/d/distribute/distribute-0.6.35.tar.gz
+curl https://pypi.python.org/packages/source/d/distribute/distribute-0.6.35.tar.gz > distribute-0.6.35.tar.gz
 tar xf distribute-0.6.35.tar.gz
 cd distribute-0.6.35
 python3 setup.py install
