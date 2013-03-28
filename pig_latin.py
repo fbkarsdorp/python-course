a4b87c635c2c5cb8cc7afaa97d4007a94eb757cc
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-03-28 08:16:35 +0100

    add imperative convert

--- pig_latin.py
+++ pig_latin.py
@@ -13,10 +13,22 @@ def add_ay(word):
     return word + 'ay'
 
 def convert_word(word):
-    "Convert a word to latin."
+    "Convert a word to latin (recursive style)."
     if not starts_with_vowel(word):
         return convert_word(word[1:] + word[0])
     return add_ay(word)
+
+def convert_word_imperative(word):
+    "Convert a word to latin (imperative style)."
+    start = 0
+    end = ''
+    for i, char in enumerate(word):
+        if char not in VOWELS:
+            end += char
+        else:
+            start = i
+            break # break out of the loop
+    return add_ay(word[start:] + end)
 
 def convert_text(text):
     "Convert all words in a sentence to latin."
