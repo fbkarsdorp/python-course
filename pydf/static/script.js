43e46b9b551f6ed8f89764356d118a262344b961
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-07-11 12:53:13 +0200

    new styling

--- pydf/static/script.js
+++ pydf/static/script.js
@@ -1,27 +1,23 @@
 $(document).ready(function(){
 
-        $("#search input").on("keydown",function(e){
-                if(e.keyCode == "13"){
-                        $("#results").html("");
-                        $("#results").addClass("loading");
-                        query = $(this).val();
-                        console.log(query);
-                        $.ajax({
-                                url:"/api",
-                                data:{"q":query},
-                                method:"POST",
-                                success:function(data){
-                                        console.log(data);
-                                        $("#results").removeClass("loading");
-                                        $("#counts").html(data.categories);
-                                        $("#results").html(data.html);
-                                },
-                                error:function(){
-                                        $("#results").removeClass("loading");
-                                        $("#results").html("oops, something went wrong...");
-                                }
-                        })
-                }
-        })
-
+  $("#search input").on("keydown",function(e){
+    if(e.keyCode == "13"){
+      $("#results").html("");
+      $("#results").addClass("loading");
+      query = $(this).val();
+      $.ajax({
+        url:"/searchbox",
+        data:{"q":query},
+        method:"POST",
+        success:function(data){
+          $("#results").removeClass("loading");
+          $("#results").html(data.html);
+        },
+        error:function(){
+          $("#results").removeClass("loading");
+          $("#results").html("oops, something went wrong...");
+        }
+      })
+    }
+  })
 })
