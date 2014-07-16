b4f20dd838860c69aefb2a134304665f2cfb34a1
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-07-16 09:19:33 +0200

    update to python 3.4

--- mac-installer.sh
+++ mac-installer.sh
@@ -2,14 +2,14 @@
 mkdir tmp
 cd tmp
 echo "Downloading Python 3"
-curl http://www.python.org/ftp/python/3.3.5/python-3.3.5-macosx10.6.dmg > python-3.3.5-macosx10.6.dmg
+curl http://www.python.org/ftp/python/3.4.1/python-3.4.1-macosx10.6.dmg > python-3.4.1-macosx10.6.dmg
 
 
 sleep 1
-hdiutil attach python-3.3.5-macosx10.6.dmg
+hdiutil attach python-3.4.1-macosx10.6.dmg
 sleep 2
 
-cd /Volumes/Python\ 3.3.5
+cd /Volumes/Python\ 3.4.1
 echo "The installer will query for your password in order to install Python 3 on your system"
 sudo installer -pkg Python.mpkg -target /
 cd -
@@ -23,14 +23,18 @@ python3 setup.py install
 
 echo "Downloading and installing iPython 3 and dependencies"
 cd ..
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 ipython
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 readline
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 tornado
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 pyzmq
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 jinja2
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 numpy
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 scipy
-/Library/Frameworks/Python.framework/Versions/3.3/bin/easy_install-3.3 matplotlib
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 ipython
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 readline
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 tornado
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 pyzmq
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 jinja2
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 numpy
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 scipy
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 matplotlib
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 seaborn
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 pandas
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 flask
+/Library/Frameworks/Python.framework/Versions/3.4/bin/easy_install-3.4 whoosh
 cd ..
 #echo "export PATH=\"/Library/Frameworks/Python.framework/Versions/3.3/bin/:\$PATH\"" >> ~/.bash_profile
 
@@ -38,13 +42,13 @@ echo -n "Do you want the installer to add Python 3 to your default path in your 
 read yn
 yn="$(tr [A-Z] [a-z] <<< "$yn")"
 if [ "$yn" == "y" ]; then
-    echo "export PATH=\"/Library/Frameworks/Python.framework/Versions/3.3/bin/:\$PATH\"" >> ~/.profile
+    echo "export PATH=\"/Library/Frameworks/Python.framework/Versions/3.4/bin/:\$PATH\"" >> ~/.profile
 else
-    echo "Python should now be installed in /Library/Frameworks/Python.framework/Versions/3.3/bin/"
+    echo "Python should now be installed in /Library/Frameworks/Python.framework/Versions/3.4/bin/"
 fi
 
 curl https://rawgithub.com/minrk/ipython_extensions/master/nbextensions/toc.js > $(ipython locate)/nbextensions/toc.js
 curl https://rawgithub.com/minrk/ipython_extensions/master/nbextensions/toc.css > $(ipython locate)/nbextensions/toc.css
 echo "IPython.load_extensions('toc');" >> $(ipython locate profile)/static/custom/custom.js
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.3/bin/:$PATH"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin/:$PATH"
