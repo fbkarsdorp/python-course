2bc5c311ad0aab4ad87c828ff728249c4d9a887b
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-07-19 10:50:38 +0200

    little more safe

--- start-unix.sh
+++ start-unix.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-source activate py34
-
 cd "$(dirname "$0")"
-which ipython3
+
+source activate py34
 if [[ $? == 0 ]]; then
-	ipython3 notebook --matploblib=inline
-else
 	ipython notebook --matplotlib=inline
+else
+	which ipython3
+	if [[ $? == 0 ]]; then
+		ipython3 notebook --matplotlib=inline
+	else
+		ipython notebook --matplotlib=inline
+	fi
 fi
