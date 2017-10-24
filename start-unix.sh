837be2c25ee4ac6176253159c23b5963c70f89ba
Tim <timjrecker@gmail.com>
2017-10-23 17:01:14 -0700

    support both jupyter and ipython notebooks

--- start-unix.sh
+++ start-unix.sh
@@ -4,12 +4,12 @@ cd "$(dirname "$0")"
 
 source activate py34
 if [[ $? == 0 ]]; then
-	jupyter-notebook
+	jupyter-notebook || ipython-notebook --matplotlib=inline
 else
 	which ipython3
 	if [[ $? == 0 ]]; then
-		jupyter-notebook
+		 jupyter-notebook || ipython-notebook --matplotlib=inline
 	else
-		jupyter-notebook
+		jupyter-notebook || ipython-notebook --matplotlib=inline
 	fi
 fi
